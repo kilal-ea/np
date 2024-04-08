ebdab87198da1a6cbd2892dc12eb890645ff443c
kilal <kilalea@gmail.com>
2024-04-08 23:53:15 +0200

    'kilal'

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('roles');
             $table->string('password');
+            $table->integer('status');
             $table->rememberToken();
             $table->timestamps();
         });
